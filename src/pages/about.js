5df47c9124bcfe79c46aebf541a20cd8a43b8cfd
uxuimike <sqrlmedia@gmail.com>
2018-12-05 13:52:35 -0500

    test change

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const About = () => (
   <Layout>
     <h1>You are so vain,</h1>
-    <p>you think this page is about you</p>
+    <p>you think this page is about you kid</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
