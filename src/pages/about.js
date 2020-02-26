83cb6f682536562757222f27e94f736c608c946b
Christophe Panon <panon_chr@hotmail.com>
2020-02-26 23:20:05 +0100

    added some style for a cv link

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,36 +7,61 @@ import SecImg2 from '../images/about/uxui.jpg'
 
 const About = () => (
   <Layout>
-    <section className='section-double'>
-      <div className='section-content'>
+    <section className="section-double">
+      <div className="section-content">
         <h3>A propos</h3>
         <h2>Résolution & méthode</h2>
-        <span className="resume">
-        <a href="/cv-français.pdf" download="cv-français.pdf" target="_blank">Download</a>
-        </span>
+        <a
+          class="btn"
+          href="/cv-français.pdf"
+          download="cv-français.pdf"
+          target="_blank"
+        >
+          <span>
+            <span>
+              <span>Consulter mon CV</span>
+            </span>
+          </span>
+        </a>
+
         <p>
-        J’apprécie l'idée de prendre quelque chose de complexe et de le décomposer en parties plus digestes, et surtout plus intelligibles. Comprendre les variables en jeu et travailler sur la solution. J'ai eu une brève vie en tant que responsable de production dans l’audiovisuel et j'ai découvert assez rapidement que ce n'était pas le genre de problèmes qui m'intéressaient. Par la suite, je me suis lancé dans la gestion de projets digitaux pour différentes entités. Bien qu'il soit plus proche de ce que je cherchais, il manquait encore quelque chose. 
+          J’apprécie l'idée de prendre quelque chose de complexe et de le
+          décomposer en parties plus digestes, et surtout plus intelligibles.
+          Comprendre les variables en jeu et travailler sur la solution. J'ai eu
+          une brève vie en tant que responsable de production dans l’audiovisuel
+          et j'ai découvert assez rapidement que ce n'était pas le genre de
+          problèmes qui m'intéressaient. Par la suite, je me suis lancé dans la
+          gestion de projets digitaux pour différentes entités. Bien qu'il soit
+          plus proche de ce que je cherchais, il manquait encore quelque chose.
         </p>
         <p>
-        Le tournant est venu lorsque j'ai découvert l'UX. Je savais que c'était le genre de problèmes que je voulais résoudre. En prenant les compétences créatives que j'avais développées et en les mettant dans le processus structuré de conception <strong>tournée vers et avec</strong> l'utilisateur, j'ai pu réconcilier les deux côtés de mon cerveau !
+          Le tournant est venu lorsque j'ai découvert l'UX. Je savais que
+          c'était le genre de problèmes que je voulais résoudre. En prenant les
+          compétences créatives que j'avais développées et en les mettant dans
+          le processus structuré de conception{' '}
+          <strong>tournée vers et avec</strong> l'utilisateur, j'ai pu
+          réconcilier les deux côtés de mon cerveau !
         </p>
       </div>
-      <div className='section-img'>
-        <img src={SecImg1} alt="UX/UI" />  
+      <div className="section-img">
+        <img src={SecImg1} alt="UX/UI" />
       </div>
     </section>
-    <section className='section-double section-reverse'>
-      <div className='section-content'>
+    <section className="section-double section-reverse">
+      <div className="section-content">
         <h3>Vision du design</h3>
         <h2>Comprendre & itérer</h2>
         <p>
-        I believe the more time you spend examining the problem, the better chance you have of solving it.  In design, asking ‘what do you need it to do’ 
-        is not enough.  You need to understand what, when, how and most importantly why.  I believe design is measured on how well it serves its 
-        purpose.  You have to walk a mile in the shoes of the users before you can truly understand what they really need. 
+          I believe the more time you spend examining the problem, the better
+          chance you have of solving it. In design, asking ‘what do you need it
+          to do’ is not enough. You need to understand what, when, how and most
+          importantly why. I believe design is measured on how well it serves
+          its purpose. You have to walk a mile in the shoes of the users before
+          you can truly understand what they really need.
         </p>
       </div>
-      <div className='section-img'>
-        <img src={SecImg2} alt="UX/UI" />  
+      <div className="section-img">
+        <img src={SecImg2} alt="UX/UI" />
       </div>
     </section>
   </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,73 +2,87 @@ import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout/layout'
-import compStyles from "./css/home.module.css"
+import compStyles from './css/home.module.css'
 
-import IntroBG from '../images/DSC_0561.jpg';
+import IntroBG from '../images/DSC_0561.jpg'
 import ContactIcon from '../images/icons/mail(1).png'
 import WorkIcon from '../images/icons/pencil(1).png'
 import AboutIcon from '../images/icons/sunglasses(1).png'
 
 class IndexPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {showModal: false};
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props)
+    this.state = { showModal: false }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-    handleClick() {
-        this.setState(prevState => ({
-            showModal: !prevState.showModal
-        }));
-    }
+  handleClick() {
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+    }))
+  }
 
-    render() {
-    
+  render() {
     return (
       <Layout>
         <section className={compStyles.intro}>
-          <div className='section-content'>
+          <div className="section-content">
             <h1>Christophe Panon</h1>
             <h2>User Experience / Product Development</h2>
             <p>
-              Over the past 16 years of my career, I have worked with many different clients, companies, and corporations. 
-              I’ve sketched, wireframed, prototyped, and tested. I’ve coded, integrated, and debugged. I’ve been a one-man show, 
-              a team leader, and have built a practice. In all of these endeavors, the common goal has been simply: to build better user experiences.
+              Over the past 16 years of my career, I have worked with many
+              different clients, companies, and corporations. I’ve sketched,
+              wireframed, prototyped, and tested. I’ve coded, integrated, and
+              debugged. I’ve been a one-man show, a team leader, and have built
+              a practice. In all of these endeavors, the common goal has been
+              simply: to build better user experiences.
             </p>
-            </div>
-            <div className='section-img'>
-            <img src={IntroBG} alt="Christophe Panon - User Experience - Gestion de projet/produit" />
+            {/* <a
+              class="btn"
+              href="/cv-français.pdf"
+              download="cv-français.pdf"
+              target="_blank"
+            >
+              <span>
+                <span>
+                  <span>Consulter mon CV</span>
+                </span>
+              </span>
+            </a> */}
+          </div>
+          <div className="section-img">
+            <img
+              src={IntroBG}
+              alt="Christophe Panon - User Experience - Gestion de projet/produit"
+            />
           </div>
         </section>
         <div className={compStyles.todo}>
-          <Link to="/portfolio" >
+          <Link to="/portfolio">
             <div className={compStyles.block}>
               <img src={WorkIcon} alt="Portfolio Icon" />
               <h3>Portfolio</h3>
-              See some of my work, projects, and designs.
+              Voir quelques uns de mes projets.
             </div>
           </Link>
-          <Link to="/about" >
+          <Link to="/about">
             <div className={compStyles.block}>
               <img src={AboutIcon} alt="Blog Icon" />
               <h3>A propos</h3>
-                Read about me, my career, and my approach to design.
+              Références et expériences passées, CV.
             </div>
           </Link>
-          <Link to="/contact" >
+          <Link to="/contact">
             <div className={compStyles.block}>
               <img src={ContactIcon} alt="Contact Icon" />
               <h3>Contact</h3>
-              Interested in working together? Let's connect.
+              Envie de me contacter ? Envoyez un email.
             </div>
           </Link>
         </div>
       </Layout>
-    );
-    }
+    )
+  }
 }
 
-export default IndexPage;
-
-
-
+export default IndexPage
