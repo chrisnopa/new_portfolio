361aaaf71d6de673ce6a21f2883b8e406fb95f6f
Christophe Panon <panon_chr@hotmail.com>
2020-03-11 19:39:57 +0100

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
       },
     },
     `gatsby-plugin-sitemap`
